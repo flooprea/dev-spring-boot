c16437813f402ddfeeabde5328a37b8aae8d1d66
flooprea <opreaf8@gmail.com>
2024-05-31 16:42:21 +0100

    test bean scopes

--- src/main/java/com/example/demo/rest/FunRestController.java
+++ src/main/java/com/example/demo/rest/FunRestController.java
@@ -1,6 +1,7 @@
 package com.example.demo.rest;
 
 import com.example.demo.model.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,15 +10,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class FunRestController {
 
     private Coach myCoach;
+    private Coach anotherCoach;
 
-
-    public FunRestController(@Qualifier("baseballCoach") Coach myCoach) {
-        this.myCoach = myCoach;
+    @Autowired
+    public FunRestController(@Qualifier("cricketCoach") Coach theCoach,
+                             @Qualifier("cricketCoach") Coach theAnotherCoach) {
+        myCoach = myCoach;
+        anotherCoach = anotherCoach;
     }
 
     @GetMapping(path = "/dailyworkout")
     private String secondFunction() {
         return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Comparing beans " + (myCoach == anotherCoach);
     }
 
     @GetMapping(path = "/fortune")
