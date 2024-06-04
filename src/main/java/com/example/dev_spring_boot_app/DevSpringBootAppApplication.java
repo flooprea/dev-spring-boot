87f3a478a8c958be6661e400b6fc3d9902cf4ed8
flooprea <opreaf8@gmail.com>
2024-06-04 22:06:07 +0100

    add missing main file and sql scripts

---
+++ src/main/java/com/example/dev_spring_boot_app/DevSpringBootAppApplication.java
@@ -0,0 +1,33 @@
+package com.example.dev_spring_boot_app;
+
+import com.example.dev_spring_boot_app.dao.StudentDAO;
+import com.example.dev_spring_boot_app.entity.Student;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DevSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevSpringBootAppApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+		return runner -> {
+			createStudent(studentDAO);
+		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+
+		Student student = new Student("Steve", "Stevenson", "setevenson@mail.com");
+
+		studentDAO.save(student);
+
+		System.out.println(student.toString());
+	}
+
+}
---
+++ src/test/java/com/example/dev_spring_boot_app/DevSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dev_spring_boot_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
